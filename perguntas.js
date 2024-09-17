f14583d73ae27b899b7857e78e8a3e3355ea0e07
Gracieli da Costa Izabel <143838084+gracyizabel@users.noreply.github.com>
2024-09-17 16:06:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    ' O que são design patterns ?',
+    'São soluções reutilizáveis para problemas comuns'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os cinco continentes ?',
+    'África, Ámerica, Antártica, Ásia, e Europa'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a importância do versionamento de código?',
+    'O versionamento de código é uma prática fundamental no desenvolvimento de software que traz várias vantagens importantes.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz minha irmã em Inglês?',
+    'Minha irmã em ingles é my sister'
+)
